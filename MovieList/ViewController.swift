eb22efedc350eda1215a9516444d7ddb173f35e6
Arian Mohajer <amohajer45@gmail.com>
2022-01-19 09:12:50 -0700

    first commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Arian Mohajer on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
